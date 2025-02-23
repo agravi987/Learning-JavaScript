e18b8bc823fef67f673fc98a8ee8256a9d34dfe0
Ravi Agrahari <152021997+agravi987@users.noreply.github.com>
2025-02-23 17:28:39 +0000

    learnt data-types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+
+
+const accountId = 2  ; 
+let accountName = "Ravi Agrahari"; 
+var accountEmail = "ravi@gmail.com" ; 
+/*
+Abovid the use of var 
+- because var have issue with block scope and function scope
+*/
+let accountCity = "Parasi" ;
+
+console.log(accountName);
+console.table([accountId, accountName, accountEmail , accountCity])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" ;  // treat all code as newer version of js
+
+// alert("this is alert") // we are using node , so this will not works 
+
+let name = "Ravi" // string 
+let age = 20  // number 
+let isLoggedIn = false // boolean
+
+
+/*
+    number => range upto 2^53
+    bigInt 
+    string => ""
+    boolean => true/false 
+    null => standalone value
+    undefined => declared but not assigned/defined
+    symbol => for uniqueness
+
+*/
+
+//Object 
+
+console.log(typeof null)
+console.log(typeof []) // array
+console.log(typeof {})  // object / document
